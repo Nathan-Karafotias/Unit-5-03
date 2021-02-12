6db78378751e9b44743abb23c84a9da115683fb8
NathanKarafotia <nathan.karafotias@ocsbstudent.ca>
2021-02-12 13:47:34 +0000

    Initial commit

---
+++ script.js
@@ -0,0 +1,6 @@
+document.getElementById("buton").addEventListener("click",buttonon);
+
+function buttonon() {
+alert("Hello")
+document.getElementById("background").style.backgroundColor = "blueviolet"
+}
